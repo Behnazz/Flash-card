187b7d7a9ebbd71cb98177557d2c291adc98c7f5
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-01 14:41:37 +0100

    flash cards

--- script.js
+++ script.js
@@ -35,9 +35,7 @@ const createCards = async () => {
 }
 
 // create a single card in the DoM
-
 const createCard = (data, index) => {
-
   const card = document.createElement('div');
   card.classList.add('card');
   if (index === 0) {
@@ -57,10 +55,76 @@ const createCard = (data, index) => {
     </section>
   </section>
   `;
+
+  card.addEventListener('click', () => {
+    card.classList.toggle('show-answer')
+  })
   //add to DOM 
   cardsEl.push(card);
-
   cardContainer.appendChild(card);
+
+  updateCurrentText();
+
 };
 
+//show numbers of cards
+const updateCurrentText = () => {
+  currentEl.innerText = ` ${currentActiveCard + 1}/${cardsEl.length}`
+}
+
 createCards()
+
+//next button event listener
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+
+//previous button event listener
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+  currentActiveCard = currentActiveCard - 1;
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  
+  updateCurrentText();
+});
+
+// show add container
+showBtn.addEventListener('click', () => {
+  addContainer.classList.add('show')
+});
+
+//hide add container
+hideBtn.addEventListener('click', () => {
+  addContainer.classList.remove('show');
+});
+
+//add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+ 
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+    createCard(newCard);
+    questionEl.value = '';
+    answerEl.value = '';
+    addContainer.classList.remove('show');
+  };
+});
+
+clearBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].innerHTML = '';
+
+  updateCurrentText() -1;
+})
